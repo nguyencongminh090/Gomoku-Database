8a699d9741f86de53eacc4bf3e5628d03f3b15b6
Nguyen Cong Minh <nguyencongminh050@gmail.com>
2021-11-26 22:29:13 +0700

    Update

--- Search_tree.py
+++ Search_tree.py
@@ -25,7 +25,7 @@ class Node:
     def get_move(self, lst: list):
         child = [i.node for i in self.child]
         return 'Not found' if len(lst) != 0 and lst[0] not in child else self.child[child.index(lst[0])].get_move(lst[1:]) \
-            if len(lst) != 0 else child if len(child) > 1 else child[0]
+            if len(lst) != 0 else child if len(child) != 0 else 'Empty'
 
     def set_depth(self, lst: list, depth):
         child = [i.node for i in self.child]
@@ -57,14 +57,21 @@ class Node:
             self.val = str(note)
         return
 
+    def __str__(self, level=-1):
+        ret = "    "*level+str(self.node)+"\n"
+        for child in self.child:
+            ret += child.__str__(level+1)
+        return ret
 
-tree = Node()
+    def __repr__(self):
+        return '<tree node>'
+
+tree = Node('Tree Node')
 tree.add(['h3', 'e8', 'm5'])
 tree.add(['h3', 'e8', 'm6'])
 tree.add(['h3', 'm4', 'm5'])
 tree.add(['k5', 'c3', 'l4'])
 tree.add(['k5', 'c1', 'l4'])
 
-print(tree.get_move(['h3', 'e8']))
-print(tree.get_move(['k5']))
-print(tree.get_move(['a5']))
+print(str(tree))
+
